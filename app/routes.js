8e770072b05a280e84f66612294842450da9a168
Rebecca Snyder <snyderrc@gmail.com>
2016-08-12 16:42:54 -0700

    missed one merge conflict sorry

--- app/routes.js
+++ app/routes.js
@@ -4,17 +4,15 @@ import MasterPage from './components/MasterPage';
 import HomeContainer from './components/home/HomeContainer';
 import AboutContainer from './components/about/AboutContainer';
 import ContactContainer from './components/contact/ContactContainer';
-<<<<<<< HEAD
 import ResourcesContainer from './components/resources/ResourcesContainer';
-=======
 import ForumContainer from './components/forum/ForumContainer';
 import ServicesContainer from './components/services/ServicesContainer';
 import EventsContainer from './components/events/EventsContainer';
->>>>>>> 2e4d149248ec37f61647b8184caae211113bbe71
+
 
 export default(
     <Route path='/' component={MasterPage}>
-    
+
         <IndexRoute component={HomeContainer}/>
         <Route path='services' component={ServicesContainer}/>
         <Route path='events' component={EventsContainer}/>
