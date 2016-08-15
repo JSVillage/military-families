67f6de7d69bf00893e90cca50a14e2dbf11e1f1f
Aaron <airfish9@gmail.com>
2016-08-15 15:19:02 -0700

    added coverr video and relevant files

--- app/components/about/AboutPage.js
+++ app/components/about/AboutPage.js
@@ -9,11 +9,11 @@ class AboutPage extends Component {
     render() {
         return <div>
         	
-        	<div className="container">
+        	<div className="container about-page">
         		<h1>About Us</h1>
             	
             	<div className="row">
-            		<div className="col-md-6">
+            		<div className="col-md-6 about-page">
             			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac tortor quis ligula lacinia ullamcorper. Aenean ac orci lectus. Duis nisi nunc, fringilla eget porttitor sed, convallis vel nisl. Aliquam nulla neque, congue id neque a, imperdiet rutrum mauris. Suspendisse accumsan neque eget justo efficitur pretium. Nulla scelerisque felis eget augue interdum accumsan. Integer rhoncus mauris vitae volutpat cursus. Vivamus condimentum odio leo, nec facilisis felis tempor quis. Mauris sapien orci, faucibus nec purus sit amet, volutpat sodales sapien. Phasellus nec tempor elit. Vivamus at accumsan nunc. Proin ac quam lectus. Phasellus tincidunt arcu ac interdum ultrices.</p>
             			<p>Nam mauris nunc, volutpat in felis vel, dapibus ullamcorper ex. Pellentesque varius augue libero. Nam rhoncus libero eget dui lacinia feugiat. In non mattis ipsum, eget condimentum ante. Nam euismod, libero in maximus placerat, tortor velit euismod tellus, sed bibendum justo nisi tristique nunc. Phasellus feugiat ornare efficitur. Morbi convallis in ex vitae congue. Praesent consequat elit risus, non consequat orci placerat vehicula.</p>
             			<p>Phasellus tincidunt vitae mauris nec dapibus. Pellentesque at libero eget massa ultricies porta nec non ex. Vivamus eget pretium velit. Donec porta, nulla sit amet dapibus bibendum, metus risus ullamcorper elit, id sollicitudin nisl felis eget lacus. Nullam luctus libero et facilisis blandit. Sed posuere elit a maximus commodo. Donec non porttitor erat, eget tempor nulla. Nunc consequat convallis lacus vel condimentum. Mauris a tincidunt tortor. Proin tincidunt turpis hendrerit elementum eleifend. Duis hendrerit quam neque, quis auctor ante posuere sed. Nulla ac placerat purus.</p>
--- app/components/home/HomePage.js
+++ app/components/home/HomePage.js
@@ -8,31 +8,44 @@ class HomePage extends Component {
 
     render() {
         return <div>
-        <div className="jumbotron">
-        	<img className="img-responsive" src="http://placehold.it/1903x650"/>
-        	<div id="search-wrapper">
-    			<div className="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
-    				<form className="form-inline">
-    					<input className="form-control" type="text" id="search-field" placeholder="What are you looking for?"/>
-    					<button type="submit" className="btn btn-primary" id="search-button">Search</button>
-		        	</form>
+        <div className="homepage-hero-module">
+		    <div className="video-container">
+		        <div className="filter"></div>
+		        <video autoPlay loop className="fillWidth">
+		            <source src="app/coverr-video/Walking-By.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+		            <source src="app/coverr-video/Walking-By.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+		        </video>
+		        <div className="poster hidden">
+		            <img src="app/coverr-video/Walking-By.jpg" alt="" />
+		        </div>
+		        <div className="title-container">
+		        	<h1>Military Veterans</h1>
+		        	<h3>A recource for PTSD victims</h3>
 	        	</div>
-        	</div>
-        </div>
-
+		        <div id="search-wrapper">
+	    			<div className="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
+	    				<form className="form-inline">
+	    					<input className="form-control" type="text" id="search-field" placeholder="What are you looking for?"/>
+	    					<button type="submit" className="btn btn-primary" id="search-button">Search</button>
+			        	</form>
+		        	</div>
+	        	</div>
+		    </div>
+		</div>
+  
         <div className="container">
 			<div className="row">
-				<div className="col-md-4">
+				<div className="col-md-4 home-page">
 					<h2><i className="fa fa-hospital-o" aria-hidden="true"></i> Crisis</h2>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
 				</div>
-				<div className="col-md-4">
+				<div className="col-md-4 home-page">
 					<h2><i className="fa fa-medkit" aria-hidden="true"></i> Help</h2>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
 				</div>
-				<div className="col-md-4">
+				<div className="col-md-4 home-page">
 					<h2><i className="fa fa-users" aria-hidden="true"></i> Socialize</h2>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
 					<p>Quisque rutrum sem at tempus fringilla. Pellentesque quis fringilla tortor. Praesent fringilla maximus magna sed maximus. Maecenas quis pellentesque ante. Cras tempus finibus ex nec tempus. Sed eleifend diam id tincidunt imperdiet. Vivamus euismod et elit id maximus. Donec efficitur posuere mi, sed feugiat ligula pulvinar sit amet.</p>
