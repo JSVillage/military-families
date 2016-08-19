eee4fa8a85fc1b0f2f8b74ae9d972a29226c947c
Moe Lubovac <mlubovac@ubiquia.local>
2016-08-19 16:09:45 -0700

    replaced browserHistory with hashHistory

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import {Router, hashHistory} from 'react-router';
 import routes from './routes';
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+render(<Router history={hashHistory} routes={routes} />, document.getElementById('app'));
