0b87369e95f97ee4e80defb8ac79c6bcd99d05b1
mlubovac <volimcajke@yahoo.com>
2016-08-20 22:54:50 -0700

    added css for leaflet container

--- app/components/events/EventsPage.js
+++ app/components/events/EventsPage.js
@@ -1,37 +1,38 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
-import EventList from './EventList';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import EventList from './EventList';
 
 class EventsPage extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
+	render() {
 
-         const position = [33.9, -112];
-        return <div>
-            <div className="col-sm-4">
-            	<EventList events={this.props.events} />
-            </div>
-            <Link to="submit-event" className="btn btn-primary">Submit Event</Link>
-            <div className="col-sm-8">
-            <Map center={position} zoom={13}>
-                <TileLayer
-                  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
-                <Marker position={position}>
-                  <Popup>
-                    <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-                  </Popup>
-                </Marker>
-              </Map>            	
-           </div>
-        </div>;
-    }
-}
+		const position = [33.9, -112];
+
+		return <div>
+				<div className="col-sm-4">
+					<EventList events={this.props.events} />
+				</div>
+				<div className="col-sm-8">
+					<Link to="submit-event" className="btn btn-primary">Submit Event</Link>
+					<Map center={position} zoom={13}>
+					<TileLayer
+						url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+						<Marker position={position}>
+							<Popup>
+								<span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+							</Popup>
+						</Marker>
+					</Map>            	
+				</div>
+			</div>;
+		}
+	}
 
 EventsPage.propTypes = {
 	events: PropTypes.array.isRequired
