a919f558bd9f03182d5b65de017256899fe2537a
amfische <amfischer21@gmail.com>
2016-08-23 11:35:52 -0700

    styled Service page headers

--- app/components/services/ServicesPage.js
+++ app/components/services/ServicesPage.js
@@ -7,27 +7,22 @@ class ServicesPage extends Component {
     }
 
     render() {
-        return <div>
-            <h1>Services Page </h1>
-            <div className="container">
-                <div className="row text-center">
-                    <div className="list-group">
-                        <a href="#" className="list-group-item">What's this for?</a>
-                        <div className="col-md-3">
-                            What is this container for?
-                        </div>
-                        <div className="col-md-6">
-                            {
-                                this.props.services.map((service, index) =>
-                                    <div key={index}> 
-                                        <p>{service.program}</p>
-                                        <p>{service.facilityName}</p>
-                                        <p>{service.address}</p> 
-                                        <p>display additional properties such as city, state, etc...</p>                                                                               
-                                    </div>
-                                )
-                            }
-                        </div>
+        return <div className="services">
+            <h1>PTSD Services in Arizona</h1>
+            <p>The following is a list of PTSD programs recognized by the Department of Veterans Affairs</p>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        {
+                            this.props.services.map((service, index) =>
+                                <div key={index}> 
+                                    <p>{service.program}</p>
+                                    <p>{service.facilityName}</p>
+                                    <p>{service.address}</p> 
+                                    <p>display additional properties such as city, state, etc...</p>                                                                               
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
