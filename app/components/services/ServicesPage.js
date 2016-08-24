2347444b0e905c21024d2e42c4a3b0bee4c6324f
amfische <amfischer21@gmail.com>
2016-08-23 17:27:59 -0700

    styling changes

--- app/components/services/ServicesPage.js
+++ app/components/services/ServicesPage.js
@@ -12,8 +12,8 @@ class ServicesPage extends Component {
         return <div className="services">
             <div id="services-header">
                 <h1>PTSD Service Providers in Arizona</h1>
-                <p>The following is a list of PTSD programs recognized by the Department of Veterans Affairs. According to the VA's website, all VA Medical Centers offer PTSD treatment, 
-                even if there is no specific PTSD program. Many Vet Centers and VA Community Based Outpatient Clinics also offer PTSD treatment. 
+                <p>The following is a list of PTSD programs recognized by the Department of Veterans Affairs. According to the VA's website, <i>"All VA Medical Centers offer PTSD treatment, 
+                even if there is no specific PTSD program... Many Vet Centers and VA Community Based Outpatient Clinics also offer PTSD treatment." </i> 
                 Contact one of the medical centers listed below and ask for the Mental Health Clinic. A full list of Arizona's clinics, Vet Centers and Medical Centers can be found 
                 <a href="http://www.va.gov/directory/guide/state.asp?STATE=AZ&dnum=ALL" target="_blank"> here</a>. 
                 A nationwide list of VA Medial Centers can be found <a href="http://www.va.gov/directory/guide/home.asp" target="_blank">here</a>.</p>
